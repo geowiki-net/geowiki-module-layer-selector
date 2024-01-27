e4a123fa44c3b5a824d64abf2de447a05f718fc6
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2024-01-27 16:49:40 +0100

    Layer Selector: Show style/data ids of layers

--- src/layer-selector.js
+++ src/layer-selector.js
@@ -6,38 +6,64 @@ module.exports = {
   id: 'layer-selector',
   requireModules: ['layers'],
   appInit (app) {
-    app.map.addControl(new LayerSelectorControl())
+    app.map.addControl(new LayerSelectorControl(app))
   }
 }
-
-let layerSelectorWindow
 
 const LayerSelectorControl = L.Control.extend({
   options: {
     position: 'topright'
     // control position - allowed: 'topleft', 'topright', 'bottomleft', 'bottomright'
   },
+  initialize: function (app) {
+    this.app = app
+  },
   onAdd: function (map) {
     const container = L.DomUtil.create('div', 'leaflet-bar leaflet-control-layer-selector')
     container.innerHTML = "<a href='#'><i class='fa fa-layer-group'></i></a>"
     container.title = modulekitLang.lang('layer-selector')
 
-    container.onclick = function () {
-      if (layerSelectorWindow) {
-        layerSelectorWindow.show()
+    container.onclick = () => {
+      if (this.window) {
+        this.window.show()
         return
       }
 
-      layerSelectorWindow = new Window({
+      this.window = new Window({
         title: modulekitLang.lang('layer-selector')
       })
 
-      layerSelectorWindow.show()
+      this.window.show()
+
+      this.showLayers()
 
       return false
     }
 
     return container
+  },
+  showLayers () {
+    const ul = document.createElement('ul')
+
+    this.app.layers.forEach(layer => {
+      const li = document.createElement('li')
+
+      const layerName = document.createElement('div')
+      layerName.appendChild(document.createTextNode(layer.styleFile))
+      li.appendChild(layerName)
+
+      const dataName = document.createElement('div')
+      dataName.appendChild(document.createTextNode(layer.data))
+      li.appendChild(dataName)
+
+      ul.appendChild(li)
+    })
+
+    const current = this.window.content.firstChild
+    if (current) {
+      this.window.content.replaceChild(ul, current)
+    } else {
+      this.window.content.appendChild(ul)
+    }
   }
 })
-
